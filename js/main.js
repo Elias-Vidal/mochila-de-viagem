f0c6f1a185f36b45b3a000c0076a398e92704b7c
Elias <eliasjuniorfranca@hotmail.com>
2023-06-20 21:39:16 -0300

    refatoração

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 
 itens.forEach((elemento) => {
-    console.log(elemento.nome, elemento.quantidade) 
+    criaElemento(elemento)
 })
 
 
@@ -16,28 +16,18 @@ form.addEventListener("submit", (evento) => {
 
     criaElemento(nome.value, quantidade.value)
     
-    nome.value = ""
-    quantidade.value = ""
+    
+
   })
 
   function criaElemento(nome,quantidade){
    const novoItem =  document.createElement('li')
    novoItem.classList.add("item")
-
-   const numeroItem= document.createElement('strong')
-   numeroItem.innerHTML = quantidade
+    
+    const numeroItem= document.createElement('strong')
     novoItem.appendChild(numeroItem)
-
     novoItem.innerHTML += nome
 
-    lista.appendChild(novoItem)
-    const itemAtual = {
-      "nome": nome,
-      "quantidade":quantidade
-    }
-    itens.push(itemAtual)
-    localStorage.setItem("itens", JSON.stringify(itens))
-    
-
+    lista.appendChild(novoItem) 
   }
   
